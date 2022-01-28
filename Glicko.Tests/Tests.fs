ddc991c1a2dbf0490015a2455a91796e201541b9
Andrew Grant <andrew.grant@codecomputerlove.com>
2022-01-28 13:04:40 +0000

    A lot of tidying

--- Glicko.Tests/Tests.fs
+++ Glicko.Tests/Tests.fs
@@ -1,7 +1,5 @@
 module Tests
 
-open System
-
 open NodaTime
 open Xunit
 open FsUnit.Xunit
@@ -10,134 +8,165 @@ open Glicko
 open Glicko.Domain
 
 [<Fact>]
-let ``Calculate c`` () =
+let ``Calculate c``() =
     let newPlayerRd = RD 350
     let typicalRd = RD 50
     let timeToBaseUncertainty = 100
 
-    RD.c newPlayerRd typicalRd timeToBaseUncertainty
-    |> should (equalWithin 0.01) 34.64
+    RD.c newPlayerRd typicalRd timeToBaseUncertainty |> should (equalWithin 0.01) 34.64
 
 [<Fact>]
-let ``Determine new RD from time passing`` () =
+let ``Determine new RD from time passing``() =
     let newPlayerRd = RD 350
     let typicalRd = RD 50
     let timeToBaseUncertainty = 100
     let elapsedDays = 10
     let currentRd = RD 200
 
-    let c =
-        RD.c newPlayerRd typicalRd timeToBaseUncertainty
+    let c = RD.c newPlayerRd typicalRd timeToBaseUncertainty
 
-    RD.updateRd c newPlayerRd elapsedDays currentRd
-    |> should equal (RD 228)
+    RD.updateRd c newPlayerRd elapsedDays currentRd |> should equal (RD 228)
 
 [<Fact>]
-let ``Calculate q`` () =
-    q |> should (equalWithin 0.000001) 0.00575646273
+let ``Calculate q``() = q |> should (equalWithin 0.000001) 0.00575646273
 
 [<Theory>]
 [<InlineData(30., 0.9955)>]
 [<InlineData(100., 0.9531)>]
 [<InlineData(300., 0.7242)>]
-let ``Calculate g`` (rd, expected) =
-    RD.g (RD rd)
-    |> should (equalWithin 0.0001) expected
+let ``Calculate g`` (rd, expected) = RD.g (RD rd) |> should (equalWithin 0.0001) expected
 
 [<Theory>]
 [<InlineData(1500, 1400, 30, 0.639)>]
 [<InlineData(1500, 1550, 100, 0.432)>]
 [<InlineData(1500, 1700, 300, 0.303)>]
 [<InlineData(1500, 1500, 45, 0.5)>]
-let ``Calculate E`` (r, rj, rdi, expected) =
-    Glicko.e (Glicko.Rating r) (Glicko.Rating rj) (RD rdi)
+let ``Calculate E`` (r, rj, rdj, expected) =
+    { rating = (Rating r)
+      rd = (RD 0) }
+    |> Player.e
+        { rating = (Rating rj)
+          rd = (RD rdj) }
     |> should (equalWithin 0.001) expected
 
 [<Fact>]
-let ``Calculate dSquared`` () =
-    let r = Glicko.Rating 1500
-
+let ``Calculate dSquared``() =
     let games =
-        [ "", Glicko.Rating 1400, RD 30, Glicko.Outcome.Won
-          "", Glicko.Rating 1550, RD 100, Glicko.Outcome.Lost
-          "", Glicko.Rating 1700, RD 300, Glicko.Outcome.Lost ]
+        [ "",
+          { rating = Rating 1400
+            rd = RD 30 }, Outcome.Won
+          "",
+          { rating = Rating 1550
+            rd = RD 100 }, Outcome.Lost
+          "",
+          { rating = Rating 1700
+            rd = RD 300 }, Outcome.Lost ]
 
-    Glicko.dSquared r games
+    { rating = Rating 1500
+      rd = RD 0 }
+    |> Player.dSquared games
     |> sqrt
     |> should (equalWithin 0.1) 231.67
 
 [<Fact>]
-let ``Calculate post period r`` () =
-    let r = Glicko.Rating 1500
+let ``Calculate post period r``() =
+    let r = Rating 1500
     let rd = RD 200
 
     let games =
-        [ "", Glicko.Rating 1400, RD 30, Glicko.Outcome.Won
-          "", Glicko.Rating 1550, RD 100, Glicko.Outcome.Lost
-          "", Glicko.Rating 1700, RD 300, Glicko.Outcome.Lost ]
+        [ { OpponentName = ""
+            OpponentRating = Rating 1400
+            OpponentRd = RD 30
+            Outcome = Outcome.Won }
+          { OpponentName = ""
+            OpponentRating = Rating 1550
+            OpponentRd = RD 100
+            Outcome = Outcome.Lost }
+          { OpponentName = ""
+            OpponentRating = Rating 1700
+            OpponentRd = RD 300
+            Outcome = Outcome.Lost } ]
 
-    Glicko.newR r rd games
-    |> should equal (Glicko.Rating 1464)
+    Glicko.newR r rd games |> should equal (Rating 1464)
 
 [<Fact>]
-let ``Calculate post period rd`` () =
-    let r = Glicko.Rating 1500
+let ``Calculate post period rd``() =
+    let r = Rating 1500
     let rd = RD 200
 
     let games =
-        [ "", Glicko.Rating 1400, RD 30, Glicko.Outcome.Won
-          "", Glicko.Rating 1550, RD 100, Glicko.Outcome.Lost
-          "", Glicko.Rating 1700, RD 300, Glicko.Outcome.Lost ]
+        [ { OpponentName = ""
+            OpponentRating = Rating 1400
+            OpponentRd = RD 30
+            Outcome = Outcome.Won }
+          { OpponentName = ""
+            OpponentRating = Rating 1550
+            OpponentRd = RD 100
+            Outcome = Outcome.Lost }
+          { OpponentName = ""
+            OpponentRating = Rating 1700
+            OpponentRd = RD 300
+            Outcome = Outcome.Lost } ]
 
     Glicko.newRd r rd games |> should equal (RD 151)
 
 [<Fact>]
-let ``Predict outcome`` () =
-    let r1 = Glicko.Rating 1400
+let ``Predict outcome``() =
+    let r1 = Rating 1400
     let rd1 = RD 80
-    let r2 = Glicko.Rating 1500
+    let r2 = Rating 1500
     let rd2 = RD 150
 
-    Glicko.expectedOutcome r1 rd1 r2 rd2
-    |> should (equalWithin 0.001) 0.376
+    Glicko.expectedOutcome r1 rd1 r2 rd2 |> should (equalWithin 0.001) 0.376
 
 [<Fact>]
-let ``Calculate rating interval`` () =
-    let r = Glicko.Rating 1500
+let ``Calculate rating interval``() =
+    let r = Rating 1500
     let rd = RD 30
 
-    Glicko.interval r rd
-    |> should equal (Glicko.Rating 1441, Glicko.Rating 1559)
+    Glicko.interval r rd |> should equal (Rating 1441, Rating 1559)
 
 [<Fact>]
-let ``Calculate player state at date`` () =
+let ``Calculate player state at date``() =
+    let newPlayerRating = Rating 1500
     let newPlayerRd = RD 350
     let typicalRd = RD 50
     let timeToBaseUncertainty = 100
 
-    let c =
-        RD.c newPlayerRd typicalRd timeToBaseUncertainty
+    let c = RD.c newPlayerRd typicalRd timeToBaseUncertainty
 
     let updateRd = RD.updateRd c newPlayerRd
 
     let records =
-        [ LocalDate(2021, 9, 29), [ "Alice", Glicko.Rating 1375, RD 30, Glicko.Outcome.Draw ]
+        [ LocalDate(2021, 9, 29),
+          [ { OpponentName = "Alice"
+              OpponentRating = Rating 1375
+              OpponentRd = RD 30
+              Outcome = Outcome.Draw } ]
           LocalDate(2021, 9, 28),
-          [ "Alice", Glicko.Rating 1400, RD 30, Glicko.Outcome.Won
-            "Eve", Glicko.Rating 1550, RD 100, Glicko.Outcome.Lost
-            "Tony", Glicko.Rating 1700, RD 300, Glicko.Outcome.Lost ] ]
+          [ { OpponentName = "Alice"
+              OpponentRating = Rating 1400
+              OpponentRd = RD 30
+              Outcome = Outcome.Won }
+            { OpponentName = "Eve"
+              OpponentRating = Rating 1550
+              OpponentRd = RD 100
+              Outcome = Outcome.Lost }
+            { OpponentName = "Tony"
+              OpponentRating = Rating 1700
+              OpponentRd = RD 300
+              Outcome = Outcome.Lost } ] ]
 
-    Glicko.calcPlayerState updateRd (Glicko.Rating 1500) (RD 350) (LocalDate(2021, 9, 28)) records
-    |> should equal (Glicko.Rating 1500, RD 350)
+    let calculate = Glicko.calcPlayerState updateRd newPlayerRating newPlayerRd
 
-    Glicko.calcPlayerState updateRd (Glicko.Rating 1500) (RD 350) (LocalDate(2021, 9, 29)) records
-    |> should equal (Glicko.Rating 1442, RD 196)
+    calculate (LocalDate(2021, 9, 28)) records |> should equal (Rating 1500, RD 350)
 
-    Glicko.calcPlayerState updateRd (Glicko.Rating 1500) (RD 350) (LocalDate(2021, 9, 30)) records
-    |> should equal (Glicko.Rating 1426, RD 173)
+    calculate (LocalDate(2021, 9, 29)) records |> should equal (Rating 1442, RD 196)
+
+    calculate (LocalDate(2021, 9, 30)) records |> should equal (Rating 1426, RD 173)
 
 [<Fact>]
-let ``Foo`` () =
+let Foo() =
     let tournament = Map.empty
 
     let add player1 player2 (date: LocalDate) outcome tournament =
@@ -145,8 +174,7 @@ let ``Foo`` () =
         let typicalRd = RD 50
         let timeToBaseUncertainty = 100
 
-        let c =
-            RD.c newPlayerRd typicalRd timeToBaseUncertainty
+        let c = RD.c newPlayerRd typicalRd timeToBaseUncertainty
 
         let updateRd = RD.updateRd c newPlayerRd
 
@@ -160,51 +188,73 @@ let ``Foo`` () =
             | Some x -> x
             | None -> List.empty
 
-        let (p1Rating, p1Rd) =
-            Glicko.calcPlayerState updateRd (Glicko.Rating 1500) (RD 350) date p1Records
+        let p1Rating, p1Rd = Glicko.calcPlayerState updateRd (Rating 1500) (RD 350) date p1Records
 
-        let (p2Rating, p2Rd) =
-            Glicko.calcPlayerState updateRd (Glicko.Rating 1500) (RD 350) date p2Records
+        let p2Rating, p2Rd = Glicko.calcPlayerState updateRd (Rating 1500) (RD 350) date p2Records
 
         let p1RecordsNew =
             p1Records
             |> Map.ofList
-            |> Map.change
-                date
-                (function
-                | None -> [ (player2, p2Rating, p2Rd, outcome) ] |> Some
-                | Some x -> (player2, p2Rating, p2Rd, outcome) :: x |> Some)
+            |> Map.change date (function
+                   | None ->
+                       [ { OpponentName = player2
+                           OpponentRating = p2Rating
+                           OpponentRd = p2Rd
+                           Outcome = outcome } ]
+                       |> Some
+                   | Some x ->
+                       { OpponentName = player2
+                         OpponentRating = p2Rating
+                         OpponentRd = p2Rd
+                         Outcome = outcome } :: x |> Some)
             |> Map.toList
-            |> List.sortByDescending (fun (x, _) -> x)
+            |> List.sortByDescending (fun (d, _) -> d)
 
         let p2RecordsNew =
             p2Records
             |> Map.ofList
-            |> Map.change
-                date
-                (function
-                | None ->
-                    [ (player1, p1Rating, p1Rd, Glicko.Outcome.invert outcome) ]
-                    |> Some
-                | Some x ->
-                    (player1, p1Rating, p1Rd, Glicko.Outcome.invert outcome)
-                    :: x
-                    |> Some)
+            |> Map.change date (function
+                   | None ->
+                       [ { OpponentName = player1
+                           OpponentRating = p1Rating
+                           OpponentRd = p1Rd
+                           Outcome = Outcome.invert outcome } ]
+                       |> Some
+                   | Some x ->
+                       { OpponentName = player1
+                         OpponentRating = p1Rating
+                         OpponentRd = p1Rd
+                         Outcome = Outcome.invert outcome } :: x |> Some)
             |> Map.toList
-            |> List.sortByDescending (fun (x, _) -> x)
+            |> List.sortByDescending (fun (d, _) -> d)
 
-        Map.add player1 p1RecordsNew tournament
-        |> Map.add player2 p2RecordsNew
+        Map.add player1 p1RecordsNew tournament |> Map.add player2 p2RecordsNew
 
     let expected =
         [ "Alice",
-          [ LocalDate(2021, 9, 29), [ "Bob", Glicko.Rating 1338, RD 292, Glicko.Outcome.Won ]
-            LocalDate(2021, 9, 28), [ "Bob", Glicko.Rating 1500, RD 350, Glicko.Outcome.Won ] ]
+          [ LocalDate(2021, 9, 29),
+            [ { OpponentName = "Bob"
+                OpponentRating = Rating 1338
+                OpponentRd = RD 292
+                Outcome = Outcome.Won } ]
+            LocalDate(2021, 9, 28),
+            [ { OpponentName = "Bob"
+                OpponentRating = Rating 1500
+                OpponentRd = RD 350
+                Outcome = Outcome.Won } ] ]
           "Bob",
-          [ LocalDate(2021, 9, 29), [ "Alice", Glicko.Rating 1662, RD 292, Glicko.Outcome.Lost ]
-            LocalDate(2021, 9, 28), [ "Alice", Glicko.Rating 1500, RD 350, Glicko.Outcome.Lost ] ] ]
+          [ LocalDate(2021, 9, 29),
+            [ { OpponentName = "Alice"
+                OpponentRating = Rating 1662
+                OpponentRd = RD 292
+                Outcome = Outcome.Lost } ]
+            LocalDate(2021, 9, 28),
+            [ { OpponentName = "Alice"
+                OpponentRating = Rating 1500
+                OpponentRd = RD 350
+                Outcome = Outcome.Lost } ] ] ]
         |> Map.ofList
 
-    add "Alice" "Bob" (LocalDate(2021, 9, 28)) Glicko.Outcome.Won tournament
-    |> add "Alice" "Bob" (LocalDate(2021, 9, 29)) Glicko.Outcome.Won
+    add "Alice" "Bob" (LocalDate(2021, 9, 28)) Outcome.Won tournament
+    |> add "Alice" "Bob" (LocalDate(2021, 9, 29)) Outcome.Won
     |> should equal expected
--- Glicko/Library.fs
+++ Glicko/Library.fs
@@ -6,8 +6,7 @@ module Glicko =
 
         let (><) f x y = f y x // flip operator
 
-        let roundOff (x: float) =
-            Math.Round(x, MidpointRounding.AwayFromZero)
+        let roundOff (x: float) = Math.Round(x, MidpointRounding.AwayFromZero)
 
         let roundOffToInt = roundOff >> int
         let pow (x: float) (y: float) = Math.Pow(x, y)
@@ -17,16 +16,25 @@ module Glicko =
         open System
         open Infrastructure
 
+        type Outcome =
+            | Won
+            | Draw
+            | Lost
+
+        module Outcome =
+            let invert =
+                function
+                | Won -> Lost
+                | Draw -> Draw
+                | Lost -> Won
+
         let q = Math.Log(10.) / 400.
 
         type RD = RD of int
 
         module RD =
             let c (RD newPlayerRd) (RD typicalRd) daysToReturnToInitialRd =
-                (square (float newPlayerRd)
-                 - square (float typicalRd))
-                / (float daysToReturnToInitialRd)
-                |> sqrt
+                (square (float newPlayerRd) - square (float typicalRd)) / (float daysToReturnToInitialRd) |> sqrt
 
             let updateRd c (RD newPlayerRd) days (RD currentRd) =
                 [ sqrt (square (float currentRd) + square c * (float days))
@@ -40,79 +48,104 @@ module Glicko =
                 let rdSquared = square (float rd)
                 let qSquared = square q
 
-                1.
-                / sqrt (1. + (3. * qSquared * rdSquared / piSquared))
+                1. / sqrt (1. + (3. * qSquared * rdSquared / piSquared))
 
-    open System
+        type Rating = Rating of int
+
+        type Player =
+            { rating: Rating
+              rd: RD }
+
+        module Player =
+            let e { rating = (Rating rj); rd = rdj } { rating = (Rating r); rd = _ } =
+                let ratingDifference = (float r) - (float rj)
+
+                1. / (1. + pow 10. ((0. - (RD.g rdj)) * ratingDifference / 400.))
+
+            let dSquared games player =
+                let qSquared = square q
+
+                let sigma player =
+                    let sigma' player (_, pj, _) =
+                        let { rating = _; rd = rdj } = pj
+
+                        let gRdj = RD.g rdj |> square
+                        let e' = player |> e pj
+                        gRdj * e' * (1. - e')
+
+                    List.map (fun opponent -> sigma' player opponent) >> List.sum
+
+                pow (qSquared * (sigma player games)) -1.0
+
+        type GameRecord =
+            { OpponentName: string
+              OpponentRating: Rating
+              OpponentRd: RD
+              Outcome: Outcome }
 
     open NodaTime
 
     open Infrastructure
     open Domain
 
-    type Rating = Rating of int
-
-    type Outcome =
-        | Won
-        | Draw
-        | Lost
-
-    module Outcome =
-        let invert =
-            function
-            | Won -> Lost
-            | Draw -> Draw
-            | Lost -> Won
-
-    let e (Rating r) (Rating rj) rdj =
-        let ratingDifference = (float r) - (float rj)
-
-        1.
-        / (1.
-           + pow 10. ((0. - (RD.g rdj)) * ratingDifference / 400.))
-
-    let dSquared r games =
-        let qSquared = square q
-
-        let sigma r =
-            let sigma' r (_, rj, rdj, _) =
-                let gRdj = RD.g rdj |> square
-                let e' = e r rj rdj
-                gRdj * e' * (1. - e')
-
-            List.map (fun opponent -> sigma' r opponent)
-            >> List.sum
-
-        pow (qSquared * (sigma r games)) -1.
-
-    let newR r (RD rd) games =
+    let newR r rd games =
         let (Rating r') = r
-        let rdSquared = float rd |> square
+        let (RD rd') = rd
 
-        let sigma r =
-            let sigma' r (_, rj, rdj, sj) =
+        let p =
+            { rating = r
+              rd = rd }
+
+        let rdSquared = float rd' |> square
+
+        let sigma p =
+            let sigma' p gameRecord =
                 let sj' =
-                    match sj with
+                    match gameRecord.Outcome with
                     | Won -> 1.
                     | Draw -> 0.5
                     | Lost -> 0.
 
-                RD.g rdj * (sj' - (e r rj rdj))
+                let pj =
+                    { rating = gameRecord.OpponentRating
+                      rd = gameRecord.OpponentRd }
 
-            List.map (fun opponent -> sigma' r opponent)
-            >> List.sum
+                let e' = p |> Player.e pj
 
-        (float r')
-        + (q / ((1. / rdSquared) + (1. / (dSquared r games))))
-          * (sigma r games)
+                RD.g gameRecord.OpponentRd * (sj' - e')
+
+            List.map (fun opponent -> sigma' p opponent) >> List.sum
+
+        let games' =
+            games
+            |> List.map (fun gameRecord ->
+                (gameRecord.OpponentName,
+                 { rating = gameRecord.OpponentRating
+                   rd = gameRecord.OpponentRd }, gameRecord.Outcome))
+
+        (float r') + (q / ((1. / rdSquared) + (1. / (Player.dSquared games' p)))) * (sigma p games)
         |> roundOffToInt
         |> Rating
 
-    let newRd r (RD rd) games =
-        let rdSquared = float rd |> square
-        let dSquared' = dSquared r games
+    let newRd r rd games =
+        let (RD rd') = rd
 
-        pow ((1. / rdSquared) + (1. / dSquared')) -1.
+        let p =
+            { rating = r
+              rd = rd }
+
+        let rdSquared = float rd' |> square
+
+        let games' =
+            games
+            |> List.map (fun gameRecord ->
+                (gameRecord.OpponentName,
+                 { rating = gameRecord.OpponentRating
+                   rd = gameRecord.OpponentRd }, gameRecord.Outcome))
+
+        let dSquared' = Player.dSquared games' p
+
+        pow ((1. / rdSquared) + (1. / dSquared')) -1.0
         |> sqrt
         |> roundOffToInt
         |> RD
@@ -128,8 +161,7 @@ module Glicko =
             |> RD
             |> RD.g
 
-        1.
-        / (1. + (pow 10. -(g' * (r1 - r2 |> float) / 400.)))
+        1. / (1. + (pow 10. -(g' * (r1 - r2 |> float) / 400.)))
 
     let interval r rd =
         let (Rating r') = r
@@ -151,7 +183,8 @@ module Glicko =
     let calcPlayerState updateRd newPlayerRating newPlayerRd (date: LocalDate) records =
         let rec calcPlayerState' r0 rd0 lastPlayed records =
             match records with
-            | (datePlayed, games) :: tail -> calcPlayerState' (newR r0 rd0 games) (newRd r0 rd0 games) datePlayed tail
+            | (datePlayed, games) :: tail ->
+                calcPlayerState' (newR r0 rd0 games) (newRd r0 rd0 games) datePlayed tail
             | [] -> (r0, rd0, lastPlayed)
 
         List.skipWhile (fun (d, _) -> d >= date) records
@@ -160,6 +193,6 @@ module Glicko =
         |> fun (rating, rd, lastPlayed) ->
             let timeSinceLastPlayed = date - lastPlayed
 
-            let degradedRd = updateRd (timeSinceLastPlayed.Days) rd
+            let degradedRd = updateRd timeSinceLastPlayed.Days rd
 
             rating, degradedRd
